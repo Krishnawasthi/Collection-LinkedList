b513192affe17763e629c88d250b1c6fb2d96d2f
Krishnawasthi <km.awasthi.77@gmail.com>
2026-08-13 17:05:28 +0530

    linkedlist

---
+++ Collection-LinkedList/src/com/linkedlist/example/ciities/LinkedListExample.java
@@ -0,0 +1,22 @@
+package com.linkedlist.example.ciities;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		
+	LinkedList<String> cityName = new LinkedList<>();
+	
+	cityName.add("BLR");
+	cityName.add("HYD");
+	cityName.add("LKO");
+	cityName.add("DEL");
+	cityName.add("CHE");
+	cityName.add("KOL");
+	
+	System.out.println(cityName);
+
+	}
+
+}
